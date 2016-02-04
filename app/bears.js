5450cdcddd4d7ac460bb07a4f32a10fc2f7554d8
Daniel Ström <D@nielstrom.se>
2016-02-04 16:45:16 +0100

    Implement a global notification service.

--- app/bears.js
+++ app/bears.js
@@ -1,5 +1,5 @@
 angular
-  .module('bears', ['ngResource', 'ngRoute'])
+  .module('bears', ['ngResource', 'ngRoute', 'notifications'])
 
   .factory('Bear', ['$resource', function ($resource) {
     return $resource('/api/bears/:id', {id: '@_id'}, {
@@ -19,8 +19,8 @@ angular
       $scope.bear = Bear.get({id: $routeParams.id});
     }])
 
-  .controller('BearEditCtrl', ['$scope', '$routeParams', '$location', 'Bear',
-    function ($scope, $routeParams, $location, Bear) {
+  .controller('BearEditCtrl', ['$scope', '$routeParams', '$location', 'notifier', 'Bear',
+    function ($scope, $routeParams, $location, notifier, Bear) {
       // Check whether to load an existing bear, or create a new one.
       if ($routeParams.id) {
         $scope.bear = Bear.get({id: $routeParams.id});
@@ -34,17 +34,21 @@ angular
           bear.$update()
             .then(function (res) {
               $location.path('/bears/' + res._id);
+              notifier.success('The bear has been updated.');
             })
             .catch(function (res) {
+              notifier.error('Could not save the bear.');
               ////console.log('Error:', res);
             });
         } else {
           // This bear is new.
           bear.$save()
             .then(function (res) {
+              notifier.success('The bear has been created.');
               $location.path('/bears/' + res._id);
             })
             .catch(function (res) {
+              notifier.error('Could not create the bear.');
               ////console.log('Error:', res);
             });
         }
---
+++ app/notifications.js
@@ -0,0 +1,43 @@
+angular
+  .module('notifications', [])
+
+  .factory('notifier', [function () {
+    var notifications = [],
+      types = ['error', 'warn', 'info', 'success'],
+      notifier = {};
+
+    notifier.notify = function (type, message) {
+      console.log(type, ':', message);
+      notifications.push({
+        type: type,
+        message: message
+      });
+    };
+
+    types.forEach(function (type) {
+      notifier[type] = notifier.notify.bind(this, type);
+    });
+
+    notifier.clear = function (index) {
+      if (typeof index !== 'undefined' && index >= 0 && index < notifications.length) {
+        notifications.length = 0;
+      }
+    };
+
+    Object.defineProperty(notifier, 'notifications', {
+      get: function () { return notifications; }
+    });
+
+    return notifier;
+  }])
+
+  .directive('notifications', ['notifier', function (notifier) {
+    return {
+      restrict: 'E',
+      scope: true,
+      template: '<ul class="notifications"> <li class="notifications__message notifications__message--{{notification.type}}" ng-repeat="notification in notifier.notifications track by $index" ng-click="notifier.clear($index)"><p>{{notification.message}}</p><span class="notifications__message__close">X</span></li></ul>',
+      link: function (scope, element, attrs) {
+        scope.notifier = notifier;
+      }
+    };
+  }]);
