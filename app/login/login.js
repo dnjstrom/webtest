1421a7e7836b197e8c7fc363e543a811603fcb31
Daniel Ström <D@nielstrom.se>
2016-02-08 08:07:43 +0100

    Style the login page, and redirect to it on authentication error

--- app/login/login.js
+++ app/login/login.js
@@ -1,6 +1,25 @@
-angular.module('app.login', [])
-  .controller('LoginCtrl', [
-    '$scope'
-  ], function ($scope) {
+angular
+  .module('app.login', [])
+
+  .config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push(['$injector', function ($injector) {
+      return $injector.get('AuthInterceptor');
+    }]);
+  }])
+
+  .factory('AuthInterceptor', ['$q', '$location', function ($q, $location) {
+    return {
+      responseError: function (response) {
+
+        if (response.status >= 400 && response.status < 500) {
+          $location.path('/login');
+        }
+
+        return $q.reject(response);
+      }
+    };
+  }])
+
+  .controller('LoginCtrl', [ '$scope', function ($scope) {
     var user = {};
-  });
+  }]);
